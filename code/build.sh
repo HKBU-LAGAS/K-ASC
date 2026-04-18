f2f53be0fbeccb4e89ed6eab6bf0139b962150ce
Jiangrh <160565073+Jrunh@users.noreply.github.com>
2026-04-18 18:42:43 +0800

    Clean up build.sh by removing comments
    Removed commented lines for clarity and streamlined the build process.

--- code/build.sh
+++ code/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# 确保已经加载了模块
-#module load gcc/11.2.0
-#g++ --version
-# 在运行cmake之前，强制删除所有CMake缓存和生成的Makefile
-#echo "--- Cleaning CMake cache and old Makefiles ---"
-#rm -rf CMakeCache.txt CMakeFiles/ Makefile cmake_install.cmake
-# 现在运行cmake，它会因为找不到缓存而重新检测编译器
 echo "--- Configuring with CMake... ---"
 cmake .
 make clean all -j16
